96f39630cd3cf47af2c8b2a0fea88b0bc9b81df7
maddyk-anjali <2ke23mc023@kleit.ac.in>
2025-07-01 16:32:09 +0530

    main commit

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def load_model():
     return model
 
 model = load_model()
-# dfghjkl ,adhu is good girl
+# dfghjkl ,adhu is good girlfghjk
 # === Class Labels (from dataset folders) ===
 class_labels = [
     "Alternaria_Leaf_Spot",
